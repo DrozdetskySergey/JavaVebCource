49820572b4e1eebf7577de7fae6c8558a47b4c8f
DrozdetskySergey <master@2867073.ru>
2022-02-20 23:56:20 +0700

    Add HelloWorld Servlet

---
+++ src/ru/academits/drozdetskiy22/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+package ru.academits.drozdetskiy22;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        OutputStream os = resp.getOutputStream();
+
+        os.write("Hello, Happy!".getBytes(StandardCharsets.UTF_8));
+        os.flush();
+        os.close();
+    }
+}
