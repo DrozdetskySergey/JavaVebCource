339fdfcb35d86a7e75d1622c031560359b4d192f
DrozdetskySergey <master@2867073.ru>
2022-02-20 23:56:51 +0700

    Add HelloWorld Servlet

--- src/ru/academits/drozdetskiy22/HelloWorldServlet.java
+++ src/ru/academits/drozdetskiy22/HelloWorldServlet.java
@@ -1,6 +1,5 @@
 package ru.academits.drozdetskiy22;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
